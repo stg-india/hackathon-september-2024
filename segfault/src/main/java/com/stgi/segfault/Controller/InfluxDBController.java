cca538c7403d0591f26604e7c118b38ac524fdd3
Jaskeerat Singh <jaskeerat56.93@gmail.com>
2024-09-29 03:29:35 +0530

    Backend api

--- segfault/src/main/java/com/stgi/segfault/Controller/InfluxDBController.java
+++ segfault/src/main/java/com/stgi/segfault/Controller/InfluxDBController.java
@@ -1,4 +1,21 @@
 package com.stgi.segfault.Controller;
 
+import com.influxdb.query.FluxTable;
+import com.stgi.segfault.Service.InfluxDBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/influxdb")
 public class InfluxDBController {
+
+    @Autowired
+    private InfluxDBService influxDBService;
+
+    @GetMapping("/query")
+    public List<FluxTable> query(@RequestParam String query) {
+        return influxDBService.queryInfluxDB(query);
+    }
 }
---
+++ segfault/src/main/java/com/stgi/segfault/SegfaultApplication.java
@@ -0,0 +1,13 @@
+package com.stgi.segfault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegfaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegfaultApplication.class, args);
+	}
+
+}
