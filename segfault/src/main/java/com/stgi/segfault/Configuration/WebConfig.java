f8cc0309f6eef038822fe1cedffb1f4723910ace
Jaskeerat Singh <jaskeerat56.93@gmail.com>
2024-09-29 06:16:47 +0530

    Stashed file

--- segfault/src/main/java/com/stgi/segfault/Configuration/WebConfig.java
+++ segfault/src/main/java/com/stgi/segfault/Configuration/WebConfig.java
@@ -1,4 +1,16 @@
 package com.stgi.segfault.Configuration;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
+
